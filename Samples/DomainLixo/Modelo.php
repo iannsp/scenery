5a3076e1c2e86540dd66eba205e6ea3493a1964e
Ivo Nascimento <iannsp@gmail.com>
2015-04-04 18:33:38 -0300

    Configure a Sample Domain and run a Scenery

---
+++ Samples/DomainLixo/Modelo.php
@@ -0,0 +1,158 @@
+<?php
+namespace Lixo;
+
+class Lixo
+{
+    
+}
+
+class Lixeiro
+{
+    private $lixos = [];
+    public function retiraNoLocal(LocalDeRetirada $local)
+    {
+        $this->lixos = $local->recolhe(rand(1,4));
+    }
+    public function armazenarNoLixao(Lixao $lixao)
+    {
+        do{
+            $lixo  = array_shift($this->lixos);
+            if (!is_null($lixo))
+                $lixao->armazena($lixo);
+        }while(!is_null($lixo));
+    }
+}
+
+class Morador
+{
+    public function dispensaLixo(LocalDeRetirada $local)
+    {
+        $quantidade = rand(0, 3);
+        $lixos = [];
+        for($i=0; $i<$quantidade; $i++)
+        {
+            $local->coloca(new Lixo());
+        }
+    }
+}
+
+
+class LocalDeRetirada
+{
+    private $lixos;
+    
+    public function quantoLixoTem()
+    {
+        return count($this->lixos);
+    }
+    public function __construct($QuantidadedeLixos=0)
+    {
+        $this->lixos = new \SplStack();
+        for($i=0; $i<$QuantidadedeLixos; $i++){
+            $this->coloca(new Lixo());
+        }
+    }
+    public function coloca(\Lixo\Lixo $lixo)
+    {
+        $this->lixos->push($lixo);
+    }
+    
+    public function recolhe($quantidade=1)
+    {
+        $result = [];
+        for($i=0; $i<$quantidade; $i++){
+            try{
+                $result[]= $this->lixos->pop();
+            }catch(\Exception $e){
+                return $result;
+            }
+        }
+        return $result;
+    }
+}
+
+class Lixao
+{
+    private $lixos;
+    
+    public function quantoLixoTem()
+    {
+        return count($this->lixos);
+    }
+
+    public function __construct($QuantidadedeLixos=0)
+    {
+        $this->lixos = new \SplStack();
+        for($i=0; $i<$QuantidadedeLixos; $i++){
+            $this->armazena(new Lixo());
+        }
+    }
+    public function armazena(Lixo $lixo)
+    {
+        return $this->lixos->push($lixo);
+    }
+}
+
+class ServicoDeLixo
+{
+    private $dataStructure;
+    private $localDeRetirada;
+    private $lixao;
+    public function __construct($dataStructure)
+    {
+        $this->dataStructure = $dataStructure;
+        $initialState = $this->dataStructure->get()['sistemaDeLixo'][1];
+        $this->localDeRetirada = new LocalDeRetirada($initialState['localDeRetirada']);
+        $this->lixao           = new Lixao($initialState['Lixao']);
+    }
+    public function coletarLixo()
+    {
+        $numeroDeLixeiros = rand(3,6);
+        $lixeiros = [];
+        for($i=0; $i<$numeroDeLixeiros; $i++)
+        {
+            $lixeiro= New Lixeiro();
+            $lixeiro->retiraNoLocal($this->localDeRetirada);
+            $lixeiro->armazenarNoLixao($this->lixao);
+        }
+        $this->persist();
+    }
+    
+    public function produzirLixo()
+    {
+        $numeroDeMoradores = rand(1,10);
+        $moradores = [];
+        for($i=0; $i<$numeroDeMoradores; $i++)
+        {
+            $morador= New Morador();
+            $morador->dispensaLixo($this->localDeRetirada);
+        }
+        $this->persist();
+    }
+    private function persist()
+    {
+        $dadosAtuais = $this->dataStructure->get(['sistemaDeLixo'=>[1]]);
+        $quantoTemlocal = $dadosAtuais['sistemaDeLixo'][1]['localDeRetirada'];
+        $quantoTemLixao = $dadosAtuais['sistemaDeLixo'][1]['Lixao'];
+        $this->dataStructure->add(
+        [
+            'sistemaDeLixo'=>
+            [
+                ['key'=>1,[
+                    'localDeRetirada'=>$this->localDeRetirada->quantoLixoTem(), 
+                    'Lixao'=>$this->lixao->quantoLixoTem()]
+                ]
+            ]
+        ]
+        );
+        
+    }
+    public function getInfo()
+    {
+        return [
+            'data'=>$this->dataStructure,
+            'localDeRetirada'=>$this->localDeRetirada,
+            'lixao'=>$this->lixao
+            ];
+    }
+}
---
+++ Samples/DomainLixo/tests/Cenarios/run.php
@@ -0,0 +1,56 @@
+<?php
+require __DIR__.'/../../Modelo.php';
+require __DIR__.'/../../../../bootstrap.php';
+
+// Um dia qualquer numa cidade qualquer
+
+use \Lixo\Lixo;
+use \Lixo\Lixeiro;
+use \Lixo\Morador;
+use \Lixo\LocalDeRetirada;
+use \Lixo\ServicoDeLixo;
+
+use Iannsp\Scenery\Scenery;
+use Iannsp\Scenery\Data;
+use Iannsp\Scenery\RunStrategy\Strategy;
+use Iannsp\Scenery\RunStrategy\Factory;
+
+use Assert\Assertion;
+$dados = new Data([
+    'sistemaDeLixo'=>
+        [
+            ['key'=>1,['localDeRetirada'=>0, 'Lixao'=>0]]
+        ]
+    ]
+    );
+
+    $scenery = New Scenery($dados);
+    $scenery->action('Produzir Lixo', function($state){
+        $umDiaQualquer = new ServicoDeLixo($state['new']);    
+        $umDiaQualquer->produzirLixo();
+    }, function($state){
+        $newStatelocalDeRetirada = $state['new']->get()['sistemaDeLixo'][1]['localDeRetirada'];
+        $oldStatelocalDeRetirada = $state['old']->get()['sistemaDeLixo'][1]['localDeRetirada'];
+        Assertion::true(($newStatelocalDeRetirada >= $oldStatelocalDeRetirada),"Coleta Esta mais rapida que geração" );
+    });
+
+    $scenery->action('Coletar Lixo', function($state){
+        $umDiaQualquer = new ServicoDeLixo($state['new']);    
+        $umDiaQualquer->coletarLixo();
+    }, function($state){
+        $newStateLixao = $state['new']->get()['sistemaDeLixo'][1]['Lixao'];
+        $oldStateLixao = $state['old']->get()['sistemaDeLixo'][1]['Lixao'];
+        Assertion::true(($newStateLixao >= $oldStateLixao),"Recolhendo Pouco Lixo" );
+    });
+
+    $rodarAte = new \Datetime();
+    $rodarAte->add(new \DateInterval("P0YT0M10S"));
+    $runnerStrategy = Factory::get(Strategy::RUN_UNTILDATE,$scenery);
+    $result = $runnerStrategy->run(['until'=>$rodarAte,'by'=>1]);
+    $info = ["localDeRetirada"=>0,"Lixao"=>0];
+    foreach ($result as $r){
+        echo "No Local de Retirada tem {$info['localDeRetirada']} Sacos de Lixo e no Lixao {$info['Lixao']}.\n";
+        $info = $r['data']->get(['sistemaDeLixo'=>[1]])['sistemaDeLixo'][1];
+//        var_dump ($info);
+    }
+    ?>
