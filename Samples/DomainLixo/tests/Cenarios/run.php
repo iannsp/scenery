73713bcf77277225b8815b8e8de90c92ed4c8c03
Ivo Nascimento <iannsp@gmail.com>
2015-04-05 00:36:10 -0300

    convert Sample to PDO

--- Samples/DomainLixo/tests/Cenarios/run.php
+++ Samples/DomainLixo/tests/Cenarios/run.php
@@ -9,6 +9,7 @@ use \Lixo\Lixeiro;
 use \Lixo\Morador;
 use \Lixo\LocalDeRetirada;
 use \Lixo\ServicoDeLixo;
+use \Lixo\LixoRepository;
 
 use Iannsp\Scenery\Scenery;
 use Iannsp\Scenery\Data;
@@ -16,41 +17,50 @@ use Iannsp\Scenery\RunStrategy\Strategy;
 use Iannsp\Scenery\RunStrategy\Factory;
 
 use Assert\Assertion;
-$dados = new Data([
-    'sistemaDeLixo'=>
-        [
-            ['key'=>1,['localDeRetirada'=>0, 'Lixao'=>0]]
-        ]
-    ]
-    );
 
-    $scenery = New Scenery($dados);
+$dsn ='sqlite:/tmp/sceneryTest.sq3';
+$pdo = new \PDO( $dsn);
+$pdo->exec('
+drop table sistemaDeLixo; 
+create table sistemaDeLixo(id integer primary key, localDeRetirada integer, Lixao integer)');
+$pdo->exec("insert into sistemaDeLixo (localDeRetirada, Lixao) values (0,0)");
+$pdo->newFromDsn = new \PDO( $dsn);
+
+    $scenery = New Scenery($pdo);
     $scenery->action('Produzir Lixo', function($state){
         $umDiaQualquer = new ServicoDeLixo($state['new']);    
         $umDiaQualquer->produzirLixo();
     }, function($state){
-        $newStatelocalDeRetirada = $state['new']->get()['sistemaDeLixo'][1]['localDeRetirada'];
-        $oldStatelocalDeRetirada = $state['old']->get()['sistemaDeLixo'][1]['localDeRetirada'];
-        Assertion::true(($newStatelocalDeRetirada >= $oldStatelocalDeRetirada),"Coleta Esta mais rapida que geração" );
+        $repoNew = new LixoRepository($state['new']);
+        $repoOld = new LixoRepository($state['old']);
+        $newStatelocalDeRetirada = $repoNew->find(1);
+        $oldStatelocalDeRetirada = $repoOld->find(1);        Assertion::true(($newStatelocalDeRetirada >= $oldStatelocalDeRetirada),"Coleta Esta mais rapida que geração" );
     });
 
     $scenery->action('Coletar Lixo', function($state){
         $umDiaQualquer = new ServicoDeLixo($state['new']);    
         $umDiaQualquer->coletarLixo();
     }, function($state){
+        $repositoryNew = new LixoRepository($state['new']);
+        $repositoryOld = new LixoRepository($state['old']);
+        var_dump($repositoryOld->find(1));
+/*
         $newStateLixao = $state['new']->get()['sistemaDeLixo'][1]['Lixao'];
         $oldStateLixao = $state['old']->get()['sistemaDeLixo'][1]['Lixao'];
         Assertion::true(($newStateLixao >= $oldStateLixao),"Recolhendo Pouco Lixo" );
+*/
     });
 
     $rodarAte = new \Datetime();
-    $rodarAte->add(new \DateInterval("P0YT3M0S"));
+    $rodarAte->add(new \DateInterval("P0YT0M6S"));
     $runnerStrategy = Factory::get(Strategy::RUN_UNTILDATE,$scenery);
-    $result = $runnerStrategy->run(['until'=>$rodarAte,'by'=>1]);
+    $result = $runnerStrategy->run(['until'=>$rodarAte,'by'=>1,'loud'=>true]);
     $info = ["localDeRetirada"=>0,"Lixao"=>0];
     foreach ($result as $r){
+/*
         echo "No Local de Retirada tem {$info['localDeRetirada']} Sacos de Lixo e no Lixao {$info['Lixao']}.\n";
         $info = $r['data']->get(['sistemaDeLixo'=>[1]])['sistemaDeLixo'][1];
 //        var_dump ($info);
+*/
     }
     ?>
